6d8fb7aae019886bdfe03e01f8135311b118d4bc
th002 <th002@th002.shgw.local>
2018-07-16 19:42:27 +0800

    乱码处理

--- CMS/src/com/greatway/manager/LiveDataManager.java
+++ CMS/src/com/greatway/manager/LiveDataManager.java
@@ -13,381 +13,381 @@ import com.greatway.page.Page;
 
 public interface LiveDataManager {
 	/**
-	 * 鏌ヨ浜嬩笟閮ㄧ剨鎺ュ伐鏃�
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param parent 鐖秈d
+	 * 查询事业部焊接工时
+	 * @param dto扩展参数类
+	 * @param parent 父id
 	 * @return
 	 */
 	List<ModelDto> getCausehour(Page page,WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鏌ヨ鍏徃鐒婃帴宸ユ椂
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param parent鐖秈d
+	 * 查询公司焊接工时
+	 * @param dto扩展参数类
+	 * @param parent父id
 	 * @return
 	 */
 	List<ModelDto> getCompanyhour(Page page,WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 椤圭洰閮ㄧ剨鎺ュ伐鏃�
-	 * @param dto鎵╁睍鍙傛暟绫�
+	 * 项目部焊接工时
+	 * @param dto扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getItemhour(Page page,WeldDto dto);
 	
 	/**
-	 * 鐒婂彛鐒婃帴宸ユ椂
-	 * @param dto鎵╁睍鍙傛暟绫�
+	 * 焊口焊接工时
+	 * @param dto扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getJunctionHous(Page page,WeldDto dto);
 	
 	/**
-	 * 浜嬩笟閮ㄥ伐鑹鸿秴鏍囩粺璁�
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param parent 鐖秈d
+	 * 事业部工艺超标统计
+	 * @param dto 扩展参数类
+	 * @param parent 父id
 	 * @return
 	 */
 	List<ModelDto> getCauseOverproof(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 椤圭洰閮ㄥ伐鑹鸿秴鏍囩粺璁�
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param id 椤圭洰id
+	 * 项目部工艺超标统计
+	 * @param dto 扩展参数类
+	 * @param id 项目id
 	 * @return
 	 */
 	List<ModelDto> getItemOverproof(WeldDto dto,BigInteger id);
 	
 	/**
-	 * 鑾峰彇褰撳墠鎵�鍖呭惈鐨勯」鐩�
-	 * @param parent 涓婄骇椤圭洰id
+	 * 获取当前所包含的项目
+	 * @param parent 上级项目id
 	 * @return
 	 */
 	List<LiveData> getAllInsf(BigInteger parent,int type);
 	
 	/**
-	 * 鑾峰彇褰撳墠璺ㄥ害鎵�鍖呭惈鐨勬椂闂�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 获取当前跨度所包含的时间
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getAllTime(Page page,WeldDto dto);
 	
 	/**
-	 * 鍏徃宸ヨ壓瓒呮爣缁熻
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 公司工艺超标统计
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getCompanyOverproof(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 瓒呮爣鏄庣粏
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 超标明细
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getDatailOverproof(Page page,WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇鏌愮剨宸ュ湪鏌愪釜鏃堕棿/鐒婃満/鐒婂彛鐨勬�诲伐鏃�
-	 * @param welderno鐒婂伐缂栧彿
-	 * @param machineno鐒婃満缂栧彿
-	 * @param junctionno鐒婂彛缂栧彿
-	 * @param time鏃堕棿
+	 * 获取某焊工在某个时间/焊机/焊口的总工时
+	 * @param welderno焊工编号
+	 * @param machineno焊机编号
+	 * @param junctionno焊口编号
+	 * @param time时间
 	 * @return
 	 */
 	int getCountTime(String welderno,String machineno,String junctionno,String time,BigInteger id);
 	
 	/**
-	 * 鑾峰彇鐒婃満瓒呮爣
-	 * @param welderno鐒婂伐缂栧彿
-	 * @param machineno鐒婃満缂栧彿
-	 * @param junctionno鐒婂彛缂栧彿
-	 * @param time鏃堕棿
+	 * 获取焊机超标
+	 * @param welderno焊工编号
+	 * @param machineno焊机编号
+	 * @param junctionno焊口编号
+	 * @param time时间
 	 * @return
 	 */
 	List<ModelDto> getjunctionoverproof(String welderno,String machineno,String junctionno,String time,BigInteger itemid);
 	
 	/**
-	 * 鑾峰彇鍏徃瓒呮椂寰呮満缁熻
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param num 瓒呮椂鐐�
+	 * 获取公司超时待机统计
+	 * @param dto 扩展参数类
+	 * @param num 超时点
 	 * @return
 	 */
 	List<ModelDto> getcompanyOvertime(WeldDto dto , String num,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇浜嬩笟閮ㄨ秴鏃跺緟鏈虹粺璁�
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param num瓒呮椂鐐�
-	 * @param parent涓婄骇id
+	 * 获取事业部超时待机统计
+	 * @param dto扩展参数类
+	 * @param num超时点
+	 * @param parent上级id
 	 * @return
 	 */
 	List<ModelDto> getCaustOvertime(WeldDto dto , String num,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇椤圭洰閮ㄨ秴鏃跺緟鏈虹粺璁�
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param num瓒呮椂鐐�
-	 * @param parent涓婄骇id
+	 * 获取项目部超时待机统计
+	 * @param dto扩展参数类
+	 * @param num超时点
+	 * @param parent上级id
 	 * @return
 	 */
 	List<ModelDto> getItemOvertime(WeldDto dto , String num);
 	
 	/**
-	 * 鑾峰彇鎵�鏈夌剨鍙�
-	 * @param parent 鎵�灞為」鐩甶d
+	 * 获取所有焊口
+	 * @param parent 所属项目id
 	 * @return
 	 */
 	List<LiveData> getJunction(BigInteger parent);
 	
 	/**
-	 * 寰呮満鏄庣粏
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param num瓒呮椂鐐�
-	 * @param parent 椤圭洰id
+	 * 待机明细
+	 * @param dto扩展参数类
+	 * @param num超时点
+	 * @param parent 项目id
 	 * @return
 	 */
 	List<ModelDto> getDetailovertime(Page page,WeldDto dto , String num,String parent);
 	
 	/**
-	 * 鍏徃璐熻嵎鐜�
-	 * @param dto鎵╁睍鍙傛暟绫�
+	 * 公司负荷率
+	 * @param dto扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getCompanyLoads(WeldDto dto,BigInteger parent);
 	
 	
 	/**
-	 * 浜嬩笟閮ㄨ礋鑽风巼
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param parent涓婄骇id
+	 * 事业部负荷率
+	 * @param dto扩展参数类
+	 * @param parent上级id
 	 * @return
 	 */
 	List<ModelDto> getCaustLoads(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 椤圭洰閮ㄨ礋鑽风巼
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param parent涓婄骇id
+	 * 项目部负荷率
+	 * @param dto扩展参数类
+	 * @param parent上级id
 	 * @return
 	 */
 	List<ModelDto> getItemLoads(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇鎵�鏈夌剨鏈�
-	 * @param parent 椤圭洰id
-	 * @param dto鎵╁睍鍙傛暟绫�
+	 * 获取所有焊机
+	 * @param parent 项目id
+	 * @param dto扩展参数类
 	 * @return
 	 */
 	List<LiveData> getMachine(BigInteger parent);
 	
 	/**
-	 * 鑾峰彇璐熻嵎鐜囨槑缁�
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param machineno鐒婃満缂栧彿
+	 * 获取负荷率明细
+	 * @param dto扩展参数类
+	 * @param machineno焊机编号
 	 * @return
 	 */
 	List<ModelDto> getDetailLoads(Page page,WeldDto dto,String machineno);
 	
 	/**
-	 * 鑾峰彇鍏徃绌鸿浇鐜�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 获取公司空载率
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getCompanyNoLoads(WeldDto dto,BigInteger parent);
 
 	/**
-	 * 鑾峰彇浜嬩笟閮ㄧ┖杞界巼
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param parent 鐖秈d
+	 * 获取事业部空载率
+	 * @param dto 扩展参数类
+	 * @param parent 父id
 	 * @return
 	 */
 	List<ModelDto> getCaustNOLoads(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇椤圭洰閮ㄧ┖杞界巼
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param parent 鐖秈d
+	 * 获取项目部空载率
+	 * @param dto 扩展参数类
+	 * @param parent 父id
 	 * @return
 	 */
 	List<ModelDto> getItemNOLoads(WeldDto dto,BigInteger parent,String equipmentno);
 	
 	/**
-	 * 鍏徃闂茬疆鐜�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 公司闲置率
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getCompanyIdle(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 浜嬩笟閮ㄩ棽缃巼
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param parent涓婄骇id
+	 * 事业部闲置率
+	 * @param dto扩展参数类
+	 * @param parent上级id
 	 * @return
 	 */
 	List<ModelDto> getCaustIdle(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 椤圭洰閮ㄩ棽缃巼
-	 * @param dto鎵╁睍鍙傛暟绫�
-	 * @param itemid椤圭洰id
+	 * 项目部闲置率
+	 * @param dto扩展参数类
+	 * @param itemid项目id
 	 * @return
 	 */
 	List<ModelDto> getItemIdle(WeldDto dto,BigInteger itemid);
 	
 	/**
-	 * 鑾峰彇椤圭洰鎵�鏈夌剨鏈烘暟閲�
-	 * @param id 椤圭洰id
+	 * 获取项目所有焊机数量
+	 * @param id 项目id
 	 * @return
 	 */
 	int getMachineCount(BigInteger id);
 	
 	/**
-	 * 鍏徃鍗曞彴璁惧杩愯鏁版嵁缁熻
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param parent 涓婄骇id
+	 * 公司单台设备运行数据统计
+	 * @param dto 扩展参数类
+	 * @param parent 上级id
 	 * @return
 	 */
 	List<ModelDto> getCompanyUse(Page page,WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 浜嬩笟閮ㄥ崟鍙拌澶囪繍琛屾暟鎹粺璁�
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param insid 椤圭洰id
+	 * 事业部单台设备运行数据统计
+	 * @param dto 扩展参数类
+	 * @param insid 项目id
 	 * @return
 	 */
 	List<ModelDto> getCaustUse(Page page,WeldDto dto,BigInteger insid);
 	
 
 	/**
-	 * 椤圭洰閮ㄥ崟鍙拌澶囪繍琛屾暟鎹粺璁�
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param insid 椤圭洰id
+	 * 项目部单台设备运行数据统计
+	 * @param dto 扩展参数类
+	 * @param insid 项目id
 	 * @return
 	 */
 	List<ModelDto> getItemUse(Page page,WeldDto dto,BigInteger insid);
 	
 	/**
-	 * 鑾峰彇鐢ㄦ埛id
+	 * 获取用户id
 	 * @return
 	 */
 	BigInteger getUserId(HttpServletRequest request);
 	
 	/**
-	 * 鑾峰彇鎵�鏈夋椂闂�
+	 * 获取所有时间
 	 * @param dto
 	 * @return
 	 */
 	List<ModelDto> getAllTimes(WeldDto dto);
 	
 	/**
-	 * 闆嗗洟鐒婃帴宸ユ椂
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团焊接工时
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getBlochour(Page page,WeldDto dto);
 	
 	/**
-	 * 闆嗗洟瓒呮爣缁熻
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团超标统计
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getBlocOverproof(WeldDto dto);
 	
 	/**
-	 * 闆嗗洟瓒呮椂寰呮満缁熻
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param num 瓒呮椂鐐�
+	 * 集团超时待机统计
+	 * @param dto 扩展参数类
+	 * @param num 超时点
 	 * @return
 	 */
 	List<ModelDto> getBlocOvertime(WeldDto dto,String num);
 	
 	/**
-	 * 闆嗗洟璐熻浇鐜�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团负载率
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getBlocLoads(WeldDto dto);
 	
 	/**
-	 * 闆嗗洟绌鸿浇鐜�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团空载率
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getBlocNoLoads(WeldDto dto);
 	
 	/**
-	 * 闆嗗洟闂茬疆鐜�
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团闲置率
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> getBlocIdle(WeldDto dto);
 	
 	/**
-	 * 闆嗗洟鍗曞彴璁惧杩愯鏁版嵁缁熻
-	 * @param dto  鎵╁睍鍙傛暟绫�
-	 * @param parent 涓婄骇鐨勭埗id
+	 * 集团单台设备运行数据统计
+	 * @param dto  扩展参数类
+	 * @param parent 上级的父id
 	 * @return
 	 */
 	List<ModelDto> getBlocUse(Page page,WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇闆嗗洟涓嬬殑鍏徃
+	 * 获取集团下的公司
 	 * @return
 	 */
 	List<LiveData> getBlocChildren();
 	
 	/**
-	 * 浜嬩笟閮ㄥ伐鏁�
-	 * @param page 鍒嗛〉
-	 * @param parent 鐖秈d
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 事业部工效
+	 * @param page 分页
+	 * @param parent 父id
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> caustEfficiency(Page page,BigInteger parent,WeldDto dto,int min,int max);
 	
 	/**
-	 * 鍏徃宸ユ晥
-	 * @param page 鍒嗛〉
-	 * @param parent 鐖秈d
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 公司工效
+	 * @param page 分页
+	 * @param parent 父id
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> companyEfficiency(Page page ,BigInteger parent,WeldDto dto,int min,int max);
 	
 	/**
-	 * 闆嗗洟宸ユ晥
-	 * @param page 鍒嗛〉
-	 * @param parent 鐖秈d
-	 * @param dto 鎵╁睍鍙傛暟绫�
+	 * 集团工效
+	 * @param page 分页
+	 * @param parent 父id
+	 * @param dto 扩展参数类
 	 * @return
 	 */
 	List<ModelDto> blocEfficiency(Page page,WeldDto dto,BigInteger parent,int min,int max);
 	
 	/**
-	 * 鑾峰彇宸ユ晥鏈�澶у�兼渶灏忓�间互鍙婂钩鍧囪法搴�
+	 * 获取工效最大值最小值以及平均跨度
 	 */
 	List<ModelDto> getEfficiencyChartNum(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鑾峰彇宸ユ晥鍥捐〃鏁版嵁
+	 * 获取工效图表数据
 	 */
 	List<ModelDto> getEfficiencyChart(WeldDto dto,BigInteger parent,int minnum,int avgnum);
 	
 	/**
-	 * 鏍规嵁id鑾峰彇鐒婂彛淇℃伅
+	 * 根据id获取焊口信息
 	 * @param id
 	 * @return
 	 */
 	WeldedJunction getWeldedJunctionById(BigInteger id);
 	
 	/**
-	 * 鑾峰彇鐒婂彛鍒嗙被
-	 * @param page 鍒嗛〉
-	 * @param parent 鐖秈d
-	 * @param material 鏉愯川
-	 * @param external_diameter 澶栧緞
-	 * @param wall_thickness 鐠у帤
-	 * @param nextExternal_diameter 涓嬫父澶栧緞
+	 * 获取焊口分类
+	 * @param page 分页
+	 * @param parent 父id
+	 * @param material 材质
+	 * @param external_diameter 外径
+	 * @param wall_thickness 璧厚
+	 * @param nextExternal_diameter 下游外径
 	 * @return
 	 */
 	List<ModelDto> getHousClassify(Page page,BigInteger parent,String searchStr);
@@ -395,86 +395,87 @@ public interface LiveDataManager {
 	List<ModelDto> getDetailNoLoads(Page page,WeldDto dto);
 	
 	/**
-	 * 鑾峰彇鐒婃満id鎬昏揪鍥犲��
-	 * @param str 鎷兼帴鐨勭剨鏈篿d鏉′欢
+	 * 获取焊机id总达因值
+	 * @param str 拼接的焊机id条件
 	 * @return
 	 */
 	BigInteger getDyneByJunctionno(String str);
 	
 	/**
-	 * 鏌ヨ瀹炴椂鏁版嵁鍏徃鐒婃満鏁伴噺
-	 * @param dto 鎵╁紶鍙傛暟绫�
-	 * @param parent 鍏徃id
+	 * 查询实时数据公司焊机数量
+	 * @param dto 扩张参数类
+	 * @param parent 公司id
 	 * @return
 	 */
 	List<ModelDto> getCompanyMachineCount(WeldDto dto,BigInteger parent);
 
 	/**
-	 * 鏌ヨ瀹炴椂鏁版嵁浜嬩笟閮�/椤圭洰閮ㄧ剨鏈烘暟閲�
-	 * @param dto 鎵╁紶鍙傛暟绫�
-	 * @param parent 浜嬩笟閮╥d
+	 * 查询实时数据事业部/项目部焊机数量
+	 * @param dto 扩张参数类
+	 * @param parent 事业部id
 	 * @return
 	 */
 	List<ModelDto> getCaustMachineCount(WeldDto dto,BigInteger parent);
 	
 	
 	/**
-	 * 鏌ヨ瀹炴椂鏁版嵁闆嗗洟鐒婃満鏁伴噺
-	 * @param dto 鎵╁紶鍙傛暟绫�
-	 * @param parent 浜嬩笟閮╥d
+	 * 查询实时数据集团焊机数量
+	 * @param dto 扩张参数类
+	 * @param parent 事业部id
 	 * @return
 	 */
 	List<ModelDto> getBlocMachineCount(WeldDto dto,BigInteger parent);
 	
 	/**
-	 * 鏍规嵁缁勭粐鏈烘瀯鍙婃椂闂寸偣鑾峰彇宸ヤ綔鎬绘椂闀�
-	 * @param parent 缁勭粐鏈烘瀯id
-	 * @param time 鏃堕棿鐐�
-	 * @param mid 鐒婃満id
+	 * 根据组织机构及时间点获取工作总时长
+	 * @param parent 组织机构id
+	 * @param time 时间点
+	 * @param mid 焊机id
 	 * @return
 	 */
 	BigInteger getCountByTime(BigInteger parent,String time,BigInteger mid);
 	
 	/**
-	 * 鏍规嵁鐒婂伐鑾峰彇鐒婂彛
-	 * @param dto 鎵╁睍鍙傛暟绫�
-	 * @param welder 鐒婂伐缂栧彿
+	 * 根据焊工获取焊口
+	 * @param dto 扩展参数类
+	 * @param welder 焊工编号
 	 * @return
 	 */
 	List<ModelDto> getJunctionByWelder(Page page,WeldDto dto ,String welder);
 	
 	/**
-	 * 鑾峰彇瓒呮爣鍥炴函
-	 * @param time 瓒呮爣鏃堕棿
-	 * @param welder 鐒婂伐
-	 * @param jucntion 鐒婂彛
+	 * 获取超标回溯
+	 * @param time 超标时间
+	 * @param welder 焊工
+	 * @param jucntion 焊口
 	 * @return
 	 */
 	List<ModelDto> getExcessiveBack(String time,String welder,String junction);
+
 	
 	/**
-	 * 鑾峰彇闆嗗洟璁惧杩愯鏃堕暱
-	 * @param page 鍒嗛〉
-	 * @param parent 缁勭粐鏈烘瀯id
-	 * @param dto dto.dtoTime1 璧峰鏃堕棿 dto.dtoTime2 缁撴潫鏃堕棿 
-	 * @param startindex (limit鍚庣殑鍙傛暟) 杩斿洖璁板綍琛岀殑鍋忕Щ閲忥紝浠�0寮�濮�
-	 * @param endindex  (limit鍚庣殑鍙傛暟) 杩斿洖璁板綍琛岀殑鏈�澶ф暟鐩�
+	 * 获取设备运行时长
+	 * @param page 分页
+	 * @param parent 上级id
+	 * @param dto dto.dtoTime1 起始时间 dto.dtoTime2 结束时间
+	 * @param startindex (limit参数) 开始下标
+	 * @param endindex  (limit参数) 结束下标
 	 * @return
 	 */
 	List<ModelDto> getBlocRunTime(Page page, BigInteger parent, WeldDto dto, int startindex, int endindex);
 	
 	/**
-	 * 鑾峰彇璁惧浣跨敤鐜�
-	 * @param time1 璧峰鏃堕棿(蹇呭～)
-	 * @param time2 缁撴潫鏃堕棿(蹇呭～)
+	 * 设备利用率
+	 * @param time1 起始时间
+	 * @param time2 结束时间
 	 * @return
 	 */
 	List<ModelDto> getUseratio(String time1,String time2);
 	
 	/**
-	 * 鑾峰彇璁惧缁翠慨鐜�
-	 * @param page 鍒嗛〉
-	 * @param dto dtoTime1 缁翠慨璧峰鏃堕棿 dtoTime2 缁翠慨缁撴潫鏃堕棿
+	 * 设备维修率
+	 * @param page 分页
+	 * @param dto dtoTime1 起始时间 dtoTime2 结束时间
 	 * @return
 	 */
 	List<ModelDto> getMaintenanceratio(Page page, WeldDto dto);
@@ -482,8 +483,8 @@ public interface LiveDataManager {
 	
 
 	/**
-	 * 鑾峰彇缁翠慨鎬绘鏁�/鎬荤淮淇垂鐢�/鎬昏澶囪垂鐢�
-	 * @param dto dtoTime1 缁翠慨璧峰鏃堕棿 dtoTime2 缁翠慨缁撴潫鏃堕棿
+	 * 获取设备维修总次数，总金额以及设备价值
+	 * @param dto dtoTime1 起始时间 dtoTime2 结束时间
 	 * @return
 	 */
 	ModelDto getSumMaintenance(WeldDto dto);
