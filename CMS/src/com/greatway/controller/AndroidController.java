75d1016f2ab4384ac4c4a64a68f99d8bc5469e25
Administrator <Administrator@th002.shgw.local>
2018-09-29 13:38:39 +0800

    位置修改

--- CMS/src/com/greatway/controller/AndroidController.java
+++ CMS/src/com/greatway/controller/AndroidController.java
@@ -9,21 +9,16 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.github.pagehelper.PageInfo;
 import com.greatway.dto.ModelDto;
 import com.greatway.dto.WeldDto;
 import com.greatway.manager.InsframeworkManager;
 import com.greatway.manager.LiveDataManager;
 import com.greatway.model.Insframework;
-import com.greatway.model.WeldedJunction;
-import com.greatway.page.Page;
 import com.greatway.util.IsnullUtil;
-import com.spring.model.MyUser;
 
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
